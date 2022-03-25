96cc3706d1f0999eb60214ccbef25b185b61c83b
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-25 13:14:19 +0400

    task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine(IsOdd(22));
+
+            
+
+           // Console.WriteLine(isEven(28));
+
+        }
+
+
+        public static int IsOdd(int num)
+        {
+            if (num%2==1)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            return num;
+        }
+
+        public static int isEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+            return number;
+        }
+
+    }
+}
