fd4cb455391831c7f63cc9dd9f356fad6206dc7d
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-25 13:17:48 +0400

    task2

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string fullName;
+            string groupNo;
+
+            do
+            {
+                Console.Write("Write your FullName: ");
+                fullName = Console.ReadLine();
+            }
+            while (!Student.CheckFullname(fullName));
+
+            do
+            {
+                Console.Write("Write your Group: ");
+                groupNo = Console.ReadLine();
+            }
+            while (!Student.CheckGroupNo(groupNo));
+
+            Console.Write("Write your age: ");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("~~~~~~~~~~~~~~~");
+
+            Student student = new Student(fullName, groupNo, age);
+            student.Info();
+
+
+
+
+        }
+
+    }
+}
---
+++ Task3/Task3/Student.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Student
+    {
+        public string FullName;
+        private string _groupNo;
+        public byte Age;
+
+
+        public string GroupNo
+        {
+            get
+            {
+                return _groupNo;
+            }
+            set
+            {
+                if (CheckGroupNo(value))
+                {
+                    _groupNo = value;
+                }
+            }
+        }
+        public Student(string fullName, string groupNo, byte age)
+        {
+            FullName = fullName;
+            GroupNo = groupNo;
+            Age = age;
+        }
+        public static bool CheckFullname(string fullName)
+        {
+            bool rightFormat = false;
+            foreach (char lett in fullName)
+            {
+                if (char.IsWhiteSpace(lett))
+                {
+                    rightFormat = true;
+                }
+                if (char.IsDigit(lett))
+                {
+                    rightFormat = false;
+                }
+            }
+            return rightFormat;
+        }
+        public static bool CheckGroupNo(string groupNo)
+        {
+            bool check = false;
+            bool hasDigit = false;
+            int count = 0;
+
+            for (int i = 0; i < groupNo.Length; i++)
+            {
+
+                if (char.IsUpper(groupNo[0]))
+                {
+                    check = true;
+                }
+                if (char.IsDigit(groupNo[i]))
+                {
+                    count++;
+                    if (count == 3)
+                    {
+                        hasDigit = true;
+                    }
+                    else
+                    {
+                        hasDigit = false;
+                    }
+                }
+
+            }
+
+            bool result = check && hasDigit;
+
+            return result;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Fullname: {FullName}\nGroup: {GroupNo}\nAge: {Age}");
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
